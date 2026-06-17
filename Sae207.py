51a65d3deb0b6415083740b91d52c12ff057b9b3
tbouzid56 <tawfikbouzid56@gmail.com>
2026-06-17 10:14:21 +0200

    new file:   .vscode/settings.json 	new file:   Sae207.py 	new file:   export_csv.csv

---
+++ Sae207.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# importer le csv dans le python
+df_formation =  pd.read_csv("export_csv.csv", sep=";") #ici, préciser le séparateur du csv permet d'évite que les cases contiennes tous des guillemets 
+# test pour voir si c'est bien dans la variable
+#print(df_formation.head())
+#print(df_formation.columns)
+
+
+
+# Passer ensuite en numpy.array les données de df_num_lannion :
+arr_formation = df_formation.to_numpy() 
+# print(arr_formation)
+
+
+taux_acces = arr_formation[:,0]
+print(taux_acces)
